7690220355546e32adaeeccc1ad31e40526263ff
moh hafiz naufal <mh3604211@gmail.com>
2024-02-05 09:44:41 +0700

    fitur mesagge

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
-console.log('javascript ready...')
-
-//! responsive navigation bar 
-function showMobileNav() {
-    var navbar = document.getElementById("navbar");
-    if (navbar.className === "navibar") {
-        navbar.className += " responsive";
-    } else {
-        navbar.className = "navibar";
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")) {
+        form.reset();
     }
-}
-
-//! Button function checking
-function showNotification(params) {
-	alert(params.toString())
-	return
 }
